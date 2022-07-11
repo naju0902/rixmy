fee513001ce8bed5f1ab3a6b21f811f0569e6358
naju0902 <nahueldeolazabal1608@gmail.com>
2022-07-11 09:44:48 -0300

    Act

--- index.php
+++ index.php
@@ -341,45 +341,8 @@
   <?php require "includes/footer.php"; ?>
   <script src="js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->
   <script type="text/javascript">
-  (function($) {
-    "use strict";
-    /* Image Slider - Swiper */
-    var imageSlider = new Swiper('.image-slider', {
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false
-      },
-      loop: false,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      spaceBetween: 30,
-      slidesPerView: 5,
-      breakpoints: {
-        // when window is <= 516px
-        516: {
-          slidesPerView: 1,
-          spaceBetween: 10
-        },
-        // when window is <= 767px
-        767: {
-          slidesPerView: 2,
-          spaceBetween: 20
-        },
-        // when window is <= 991px
-        991: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
-        // when window is <= 1199px
-        1199: {
-          slidesPerView: 4,
-          spaceBetween: 30
-        },
-      }
-    });
-  })(jQuery);
+    sidebar();
+    sliderTeam();
   </script>
 </body>
 </html>						
--- js/script.js
+++ js/script.js
@@ -26,3 +26,47 @@ function sidebar() {
     });
   });
 }
+
+// Slider team
+
+function sliderTeam() {
+  (function($) {
+    "use strict";
+    /* Image Slider - Swiper */
+    var imageSlider = new Swiper('.image-slider', {
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+      },
+      loop: false,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      spaceBetween: 30,
+      slidesPerView: 5,
+      breakpoints: {
+        // when window is <= 516px
+        516: {
+          slidesPerView: 1,
+          spaceBetween: 10
+        },
+        // when window is <= 767px
+        767: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window is <= 991px
+        991: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window is <= 1199px
+        1199: {
+          slidesPerView: 4,
+          spaceBetween: 30
+        },
+      }
+    });
+  })(jQuery);
+}
